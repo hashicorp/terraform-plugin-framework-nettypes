9baa2c3ef0ce4ecd80ad59514f3849594d8b3bdc
Austin Valle <austinvalle@gmail.com>
2023-07-21 11:30:34 -0400

    updated package name and doc

--- cidrtypes/doc.go
+++ cidrtypes/doc.go
@@ -1,6 +1,5 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-// Package nettypes contains Terraform Plugin Framework Custom Type implementations for CIDRs,
-// such as IPv4 and IPv6 CIDR strings.
+// Package cidrtypes contains Terraform Plugin Framework Custom Type implementations for IPv4 and IPv6 CIDR strings.
 package cidrtypes
