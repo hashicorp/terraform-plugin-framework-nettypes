6c4c7ba68242ddc5d9d9d5dde4723b9339196c8a
Austin Valle <austinvalle@gmail.com>
2023-07-20 20:23:21 -0400

    initial skeleton

---
+++ cidrtypes/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+// Package nettypes contains Terraform Plugin Framework Custom Type implementations for CIDRs,
+// such as IPv4 and IPv6 CIDR strings.
+package cidrtypes
---
+++ nettypes/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+// Package nettypes contains Terraform Plugin Framework Custom Type implementations for networking,
+// such as IPv4 and IPv6 address strings.
+package nettypes
